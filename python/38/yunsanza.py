a6b582e37d5faa8a12575a44e1f129ede20f64bd
umintak <zmflvkdl@naver.com>
2022-01-07 15:58:00 +0900

    python연습

---
+++ python/38/yunsanza.py
@@ -0,0 +1,16 @@
+print(False and False)
+print(True or True)
+print(not False)
+
+
+#관계연산자
+c, d = 10, 4
+
+print(c == 10)
+print(c == d)
+print(c != 4)
+print(d >= 0)
+
+#비트연산자
+print(10110 & 10011)
+print(10010 | 10101)
